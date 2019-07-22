31a57b974bc6f1a7398e271ce0b03217c4373860
asuluntas <aysunsulun@gmail.com>
2019-07-22 15:04:47 -0700

    Changed styling

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navigation = () => (
+  <ul className="navigation-bar">
+    <li>
+      <a href="mailto:aysunsulun@gmail.com">Contact Me</a>
+    </li>
+    <li>
+      <a href="https://docs.google.com/document/d/1_xY0Ea4biu7OzYHD9qJIBvkzUGVaAce9eAwLkWg3cQc/view?usp=sharing">Resume</a>
+    </li>
+    <li>
+      <a href="./projects.html">Projects</a>
+    </li>
+    <li>
+      <a href="./index.html">Home</a>
+    </li>
+  </ul>
+);
+
+
+export default Navigation;
+
+
